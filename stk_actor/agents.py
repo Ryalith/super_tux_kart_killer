6790b62c5ca58aa8b989587b1b323232bcf95432
Ryalith <quelin.lucie@gmail.com>
2025-11-29 16:50:13 +0100

    Add PPOProbabilisticActor

--- stk_actor/agents.py
+++ stk_actor/agents.py
@@ -1,24 +1,14 @@
-import random
-from collections import deque, namedtuple
 from typing import Sequence
 
-import gymnasium as gym
 import torch
 import torch.nn as nn
-from torch.distributions import (
-    Independent,
-    Normal,
-    TanhTransform,
-    TransformedDistribution,
-)
-from torchrl.modules.tensordict_module import ProbabilisticActor, ValueOperator  # noqa: F401
+from torch.distributions import MultiDiscrete
 
-from .utils import build_mlp, build_ortho_mlp
+from torchrl.modules.tensordict_module import ProbabilisticActor, ValueOperator
 
-Transition = namedtuple(
-    "Transition", ("state", "action", "next_state", "reward", "terminated", "truncated")
-)
+from tensordict.nn import TensorDictModule
 
+from .env import get_observation_vector_dim, get_action_vector_dims
 #
 # PPO Agents
 #
@@ -29,7 +19,7 @@ class PPODiscretePolicyNet(nn.Module):
         self,
         input_dim: int,
         hidden_dims: Sequence[int],
-        multi_categorical_dims: Sequence[int],
+        action_dims: Sequence[int],
         Act=nn.ReLU,
     ):
         """
@@ -51,27 +41,56 @@ class PPODiscretePolicyNet(nn.Module):
             layers.append(Act())
 
         embed_dim = hidden_dims[-1]
-        total_categories = sum(multi_categorical_dims)
+        total_categories = sum(action_dims)
         layers.append(nn.Linear(embed_dim, total_categories))
 
         self.embed = nn.Sequential(*layers)
 
-        self.heads = [nn.Softmax(mctg_dim) for mctg_dim in multi_categorical_dims]
+        self.heads = [nn.Softmax(mctg_dim) for mctg_dim in action_dims]
 
-        self.multi_categorical_dims = multi_categorical_dims
+        self.action = action_dims
         self.offsets = [0]
-        for i, mctg_dim in enumerate(multi_categorical_dims[:-1]):
+        for i, mctg_dim in enumerate(action_dims[:-1]):
             self.offsets.append(mctg_dim + self.offsets[i])
 
     def forward(self, obs) -> Sequence[torch.Tensor]:
         embedding = self.embed(obs)
-        return [
+        logits = [
             # Apply softmax
-            head(embedding[..., offset : offset + mctg_dim])
-            for head, offset, mctg_dim in zip(
-                self.heads, self.offsets, self.multi_categorical_dims
-            )
+            head(embedding[..., offset : offset + act_dim])
+            for head, offset, act_dim in zip(self.heads, self.offsets, self.action_dims)
         ]
+        return {"logits": logits}
+
+
+class DiscreteProbaActor(ProbabilisticActor):
+    def __init__(
+        self,
+        env_specs,
+        hidden_net_dims: Sequence[int],
+        Act=nn.ReLU,
+    ):
+        input_dim = get_observation_vector_dim(env_specs)
+        action_dims = get_action_vector_dims(env_specs)
+
+        policy_net = PPODiscretePolicyNet(input_dim, hidden_net_dims, action_dims, Act)
+
+        policy_module = TensorDictModule(
+            policy_net, in_keys=["observation"], out_keys=["logits"]
+        )
+
+        super().__init__(
+            module=policy_module,
+            spec=env_specs["input_spec"]["full_action_spec"]["action"],
+            in_keys=["logits"],
+            distribution_class=MultiDiscrete,
+            distribution_kwargs={
+                "nvec": torch.tensor(action_dims),
+            },
+            return_log_prob=True,
+            # we'll need the log-prob for the numerator of the importance weights
+            log_prob_key="sample_log_prob",
+        )
 
 
 class PPOValueNet(nn.Module):
@@ -84,82 +103,82 @@ class PPOValueNet(nn.Module):
 #
 
 
-class SACContinuousActor(nn.Module):
-    def __init__(self, state_dim, hidden_layers, action_dim, min_std=1e-4):
-        """Creates a new Squashed Gaussian actor for use in a SAC Algo
+# class SACContinuousActor(nn.Module):
+#     def __init__(self, state_dim, hidden_layers, action_dim, min_std=1e-4):
+#         """Creates a new Squashed Gaussian actor for use in a SAC Algo
+#
+#         TODO: Make sure this approach works for discreet outputs which isn't obvious
+#
+#         :param state_dim: The dimension of the state space
+#         :param hidden_layers: Hidden layer sizes
+#         :param action_dim: The dimension of the action space
+#         :param min_std: The minimum standard deviation, defaults to 1e-4
+#         """
+#         super().__init__()
+#         self.min_std = min_std
+#         backbone_dim = [state_dim] + list(hidden_layers)
+#         self.layers = build_mlp(backbone_dim, activation=nn.ReLU())
+#         self.backbone = nn.Sequential(*self.layers)
+#         self.last_mean_layer = nn.Linear(hidden_layers[-1], action_dim)
+#         self.last_std_layer = nn.Linear(hidden_layers[-1], action_dim)
+#         self.softplus = nn.Softplus()
+#
+#         # cache_size avoids numerical infinites or NaNs when
+#         # computing log probabilities
+#         self.tanh_transform = TanhTransform(cache_size=1)
+#
+#     def normal_dist(self, state: torch.Tensor):
+#         """Compute normal distribution given observation(s)"""
+#
+#         backbone_output = self.backbone(state)
+#         mean = self.last_mean_layer(backbone_output)
+#         std_out = self.last_std_layer(backbone_output)
+#         std = self.softplus(std_out) + self.min_std
+#         # Independent ensures that we have a multivariate
+#         # Gaussian with a diagonal covariance matrix (given as
+#         # a vector `std`)
+#         return Independent(Normal(mean, std), 1)
+#
+#     def log_prob(self, state, action):
+#         normal_dist = self.normal_dist(state)
+#         action_dist = TransformedDistribution(normal_dist, [self.tanh_transform])
+#         log_prob = action_dist.log_prob(action)
+#         return log_prob
+#
+#     def forward(self, state, stochastic=True):
+#         """Computes the action a_t and its log-probability p(a_t| s_t)
+#
+#         :param stochastic: True when sampling
+#         """
+#         normal_dist = self.normal_dist(state)
+#         action_dist = TransformedDistribution(normal_dist, [self.tanh_transform])
+#         if stochastic:
+#             # Uses the re-parametrization trick
+#             action = action_dist.rsample()
+#         else:
+#             # Directly uses the mode of the distribution
+#             action = self.tanh_transform(normal_dist.mode)
+#
+#         # This line allows to deepcopy the actor...
+#         self.tanh_transform._cached_x_y = [None, None]
+#         return action
 
-        TODO: Make sure this approach works for discreet outputs which isn't obvious
 
-        :param state_dim: The dimension of the state space
-        :param hidden_layers: Hidden layer sizes
-        :param action_dim: The dimension of the action space
-        :param min_std: The minimum standard deviation, defaults to 1e-4
-        """
-        super().__init__()
-        self.min_std = min_std
-        backbone_dim = [state_dim] + list(hidden_layers)
-        self.layers = build_mlp(backbone_dim, activation=nn.ReLU())
-        self.backbone = nn.Sequential(*self.layers)
-        self.last_mean_layer = nn.Linear(hidden_layers[-1], action_dim)
-        self.last_std_layer = nn.Linear(hidden_layers[-1], action_dim)
-        self.softplus = nn.Softplus()
-
-        # cache_size avoids numerical infinites or NaNs when
-        # computing log probabilities
-        self.tanh_transform = TanhTransform(cache_size=1)
-
-    def normal_dist(self, state: torch.Tensor):
-        """Compute normal distribution given observation(s)"""
-
-        backbone_output = self.backbone(state)
-        mean = self.last_mean_layer(backbone_output)
-        std_out = self.last_std_layer(backbone_output)
-        std = self.softplus(std_out) + self.min_std
-        # Independent ensures that we have a multivariate
-        # Gaussian with a diagonal covariance matrix (given as
-        # a vector `std`)
-        return Independent(Normal(mean, std), 1)
-
-    def log_prob(self, state, action):
-        normal_dist = self.normal_dist(state)
-        action_dist = TransformedDistribution(normal_dist, [self.tanh_transform])
-        log_prob = action_dist.log_prob(action)
-        return log_prob
-
-    def forward(self, state, stochastic=True):
-        """Computes the action a_t and its log-probability p(a_t| s_t)
-
-        :param stochastic: True when sampling
-        """
-        normal_dist = self.normal_dist(state)
-        action_dist = TransformedDistribution(normal_dist, [self.tanh_transform])
-        if stochastic:
-            # Uses the re-parametrization trick
-            action = action_dist.rsample()
-        else:
-            # Directly uses the mode of the distribution
-            action = self.tanh_transform(normal_dist.mode)
-
-        # This line allows to deepcopy the actor...
-        self.tanh_transform._cached_x_y = [None, None]
-        return action
-
-
-class SACContinuousQCritic(nn.Module):
-    def __init__(self, state_dim: int, hidden_layers: list[int], action_dim: int):
-        """Creates a new critic agent $Q(s, a)$ for use in a SAC Algo
-
-        :param state_dim: The number of dimensions for the observations
-        :param hidden_layers: The list of hidden layers for the NN
-        :param action_dim: The numer of dimensions for actions
-        """
-        super().__init__()
-        self.is_q_function = True
-        self.model = build_mlp(
-            [state_dim + action_dim] + list(hidden_layers) + [1], activation=nn.ReLU()
-        )
-
-    def forward(self, transition: Transition):
-        obs_act = torch.cat((transition.state, transition.action), dim=1)
-        q_value = self.model(obs_act).squeeze(-1)
-        return q_value
+# class SACContinuousQCritic(nn.Module):
+#     def __init__(self, state_dim: int, hidden_layers: list[int], action_dim: int):
+#         """Creates a new critic agent $Q(s, a)$ for use in a SAC Algo
+#
+#         :param state_dim: The number of dimensions for the observations
+#         :param hidden_layers: The list of hidden layers for the NN
+#         :param action_dim: The numer of dimensions for actions
+#         """
+#         super().__init__()
+#         self.is_q_function = True
+#         self.model = build_mlp(
+#             [state_dim + action_dim] + list(hidden_layers) + [1], activation=nn.ReLU()
+#         )
+#
+#     def forward(self, transition: Transition):
+#         obs_act = torch.cat((transition.state, transition.action), dim=1)
+#         q_value = self.model(obs_act).squeeze(-1)
+#         return q_value
--- stk_actor/algorithms.py
+++ stk_actor/algorithms.py
@@ -11,12 +11,18 @@
 # TODO: add tensorboard loggers to check the status of training
 
 import copy
+from pathlib import Path
 
 import gymnasium as gym
 import torch
 import torchrl
+from torchrl.collectors import SyncDataCollector
+from torchrl.data.replay_buffers import ReplayBuffer
+from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
+from torchrl.data.replay_buffers.storages import LazyTensorStorage
+from torchrl.objectives import ClipPPOLoss
+from torchrl.objectives.value import GAE
 
-from .agents import ReplayBuffer, Transition
 from .utils import (
     stk_action_tensor_to_dict,
     stk_state_dict_to_tensor,
@@ -29,12 +35,20 @@ class PPOAlgo:
         "batch_size": 128,
         "discount_factor": 0.99,
         "critic_coef": 1.0,
+        "gamma": ...,
+        "lmbda": ...,
+        "clip_epsilon": ...,
+        "entropy_eps": ...,
+        "loss_critic_type": "smooth_l1",
+        "lr": ...,
+        "max_grad_norm": ...,
     }
 
     def __init__(self, env, config: dict | None = None):
         """
         Instantiate a PPOAlgorithm to be used with discreet actions
         """
+        self.env = env
         self.config = config if config is not None else self.default_config
         self.v_critic = ...
         self.old_v_critic = copy.deepcopy(self.v_critic)
@@ -43,64 +57,90 @@ class PPOAlgo:
         self.old_ppo_actor = copy.deepcopy(self.ppo_actor)
         self.replay_buffer = ReplayBuffer(self.config.buffer_capacity)
 
-    def _fill_replay_buffer(self, max_iter: int):
-        # TODO: torch rl might have a better replay buffer implementation for parallel env exploration
-        state, *_ = self.env.reset()
+    def _train_one_step(
+        self,
+        tensordict_data,
+        advantage_module,
+        loss_module,
+        optim,
+        replay_buffer,
+        frames_per_batch,
+        sub_batch_size,
+        device,
+    ):
+        advantage_module(tensordict_data)
+        data_view = tensordict_data.reshape(-1)
+        replay_buffer.extend(data_view.cpu())
+        for _ in range(frames_per_batch // sub_batch_size):
+            subdata = replay_buffer.sample(sub_batch_size)
+            loss_vals = loss_module(subdata.to(device))
+            loss_value = (
+                loss_vals["loss_objective"]
+                + loss_vals["loss_critic"]
+                + loss_vals["loss_entropy"]
+            )
 
-        with torch.no_grad():
-            for ix in range(max_iter):
-                ix += 1
-                state_tensor = stk_state_dict_to_tensor(state)
-                action_tensor = self.ppo_actor(state_tensor)
-                next_state, reward, terminated, truncated, _ = self.env.step(
-                    action_tensor.cpu().numpy()
-                )
-                next_state_tensor = stk_state_dict_to_tensor(next_state)
+            optim.zero_grad()
+            loss_value.backward()
+            torch.nn.utils.clip_grad_norm_(
+                loss_module.parameters(), self.config.max_grad_norm
+            )
+            optim.step()
 
-                done = truncated or terminated
-                self.replay_buffer.push(
-                    Transition(
-                        state_tensor,
-                        action_tensor,
-                        next_state_tensor,
-                        torch.tensor(reward),
-                        torch.tensor(terminated),
-                        torch.tensor(truncated),
-                    )
-                )
-                state = next_state
-                if done:
-                    break
-
-    def _train_one_step(self):
-        # TODO: i'm not sure if we train on transitions or normaly ordered batches
-        transitions = self.replay_buffer.sample(self.config.batch_size)
-
-        terminated = torch.cat(transitions.terminated)
-        reward = torch.cat(transitions.reward)
-
-        # Compute V Values and resulting advantage
-        v_value = ...  # use v_agent
-        old_v_value = ...  # use old_v_agent
-        advantage = ...  # see bbrl utils gae
-
-        # Compute Critic loss with TD(0)
-        target = reward + self.config.discount_factor * old_v_value * (
-            1 - terminated.int()
+    def train(self, total_frames, frames_per_batch, sub_batch_size, device):
+        # TODO: check better collectors for parallel execution
+        collector = SyncDataCollector(
+            self.env,
+            self.ppo_actor,
+            frames_per_batch=frames_per_batch,
+            total_frames=total_frames,
+            split_trajs=False,
+            device=device,
         )
-        critic_loss = (
-            torch.nn.functional.mse_loss(v_value, target) * self.config.critic_coef
-        )
-        self.critic_optimizer.zero_grad()
-        critic_loss.backward()
-        torch.nn.utils.clip_grad_norm_(
-            self.v_agent.parameters(), self.config.max_grad_norm
-        )
-        self.critic_optimizer.step()
 
-        # Compute Actor loss
+        replay_buffer = ReplayBuffer(
+            storage=LazyTensorStorage(max_size=frames_per_batch),
+            sampler=SamplerWithoutReplacement(),
+        )
+        advantage_module = GAE(
+            gamma=self.config.gamma,
+            lmbda=self.config.lmbda,
+            value_network=self.v_critic,
+            average_gae=True,
+            device=device,
+        )
 
-    def train(self):
+        loss_module = ClipPPOLoss(
+            actor_network=self.ppo_actor,
+            critic_network=self.v_critic,
+            clip_epsilon=self.config.clip_epsilon,
+            entropy_bonus=bool(self.config.entropy_eps),
+            entropy_coef=self.config.entropy_eps,
+            # these keys match by default but we set this for completeness
+            critic_coef=self.config.critic_coef,
+            loss_critic_type=self.config.loss_critic_type,  # "smooth_l1",
+        )
+
+        optim = torch.optim.Adam(loss_module.parameters(), self.config.lr)
+
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optim, total_frames // frames_per_batch, 0.0
+        )
+
+        for i, tensordict_data in enumerate(collector):
+            loss_val = self._train_one_step(
+                tensordict_data,
+                advantage_module,
+                loss_module,
+                optim,
+                replay_buffer,
+                frames_per_batch,
+                sub_batch_size,
+                device,
+            )
+            scheduler.step()
+
+    def load_checkpoint(self, checkpoint_path: Path):
         pass
 
 
