6f8ee2e1fdf92ee3831dfda4d092578fade723f5
Ryalith <quelin.lucie@gmail.com>
2025-12-19 15:24:28 +0100

    added sac algo inside the module

--- stk_actor/learn.py
+++ stk_actor/learn.py
@@ -1,19 +1,12 @@
-from pathlib import Path
-from pystk2_gymnasium import AgentSpec
 from functools import partial
-import torch
-import inspect
 
 # Note the use of relative imports
-from .actors import Actor
-from .pystk_actor import env_name, get_wrappers, player_name
+from .algorithms import SACAlgoSB
+from .env import make_cont_env
 
 if __name__ == "__main__":
     # Setup the environment
-    SAC_config = ...
-    SAC_algo = ...
+    train_env_fn = make_cont_env
 
     # Learn
-
-    # (3) Save the actor sate
-    torch.save(SAC_algo.actor.state_dict(), ... / "pystk_actor.pth")
+    model = SACAlgoSB(train_env_fn)
